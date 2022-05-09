c47c512cbe315a842d0d8ac493cb747409b3463c
Markus Spann <spannm@outlook.de>
2022-05-09 08:42:02 +0200

    Support Java 11

--- src/main/java/com/github/hypfvieh/cli/parser/ArgumentBundle.java
+++ src/main/java/com/github/hypfvieh/cli/parser/ArgumentBundle.java
@@ -13,21 +13,46 @@ import com.github.hypfvieh.cli.parser.converter.IValueConverter;
  * @author hypfvieh
  * @since 1.0.0 - 2022-05-05
  */
-public record ArgumentBundle(
-        Map<CmdArgOption<?>, String>       knownArgs,      
-        Map<CmdArgOption<?>, List<String>> knownMultiArgs, 
-        Map<String, String>                unknownArgs,    
-        List<String>                       unknownTokens,  
-        Map<CmdArgOption<?>, String>       dupArgs,        
-        List<CmdArgOption<?>>              missingArgs,
-        Map<String, CmdArgOption<?>>       options,
-        Map<Class<?>, IValueConverter<?>>  converters
-        ) {
-    
-    public ArgumentBundle() {
-        this(new LinkedHashMap<>(), new LinkedHashMap<>(), new LinkedHashMap<>(), 
-                new ArrayList<>(), new LinkedHashMap<>(), new ArrayList<>(), 
-                new LinkedHashMap<>(), new LinkedHashMap<>());
+public final class ArgumentBundle {
+    private final Map<CmdArgOption<?>, String>       knownArgs      = new LinkedHashMap<>();
+    private final Map<CmdArgOption<?>, List<String>> knownMultiArgs = new LinkedHashMap<>();
+    private final Map<String, String>                unknownArgs    = new LinkedHashMap<>();
+    private final List<String>                       unknownTokens  = new ArrayList<>();
+    private final Map<CmdArgOption<?>, String>       dupArgs        = new LinkedHashMap<>();
+    private final List<CmdArgOption<?>>              missingArgs    = new ArrayList<>();
+    private final Map<String, CmdArgOption<?>>       options        = new LinkedHashMap<>();
+    private final Map<Class<?>, IValueConverter<?>>  converters     = new LinkedHashMap<>();
+
+    public Map<CmdArgOption<?>, String> knownArgs() {
+        return knownArgs;
+    }
+
+    public Map<CmdArgOption<?>, List<String>> knownMultiArgs() {
+        return knownMultiArgs;
+    }
+
+    public Map<String, String> unknownArgs() {
+        return unknownArgs;
+    }
+
+    public List<String> unknownTokens() {
+        return unknownTokens;
+    }
+
+    public Map<CmdArgOption<?>, String> dupArgs() {
+        return dupArgs;
+    }
+
+    public List<CmdArgOption<?>> missingArgs() {
+        return missingArgs;
+    }
+
+    public Map<String, CmdArgOption<?>> options() {
+        return options;
+    }
+
+    public Map<Class<?>, IValueConverter<?>> converters() {
+        return converters;
     }
 
 }
